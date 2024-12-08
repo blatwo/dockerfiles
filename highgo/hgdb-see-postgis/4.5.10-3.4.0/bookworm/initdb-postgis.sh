d1d9b40f7acce869ec9ccf4f197df75ad3a26416
RogerQiu <blatwo@163.com>
2024-12-09 01:35:11 +0800

    add postgis

--- highgo/hgdb-see-postgis/4.5.10-3.4.0/bookworm/initdb-postgis.sh
+++ highgo/hgdb-see-postgis/4.5.10-3.4.0/bookworm/initdb-postgis.sh
@@ -3,7 +3,9 @@
 set -e
 
 # Perform all actions as $POSTGRES_USER
-export PGUSER="$POSTGRES_USER"
+export PGUSER=sysdba
+export PGPASSWORD=${POSTGRES_PASSWORD}
+psql=(psql -dhighgo)
 
 # Create the 'template_postgis' template db
 "${psql[@]}" <<- 'EOSQL'
@@ -12,14 +14,16 @@ EOSQL
 
 # Load PostGIS into both template_database and $POSTGRES_DB
 for DB in template_postgis "$POSTGRES_DB"; do
-	echo "Loading PostGIS extensions into $DB"
-	"${psql[@]}" --dbname="$DB" <<-'EOSQL'
-		CREATE EXTENSION IF NOT EXISTS postgis;
-		CREATE EXTENSION IF NOT EXISTS postgis_topology;
-		-- Reconnect to update pg_setting.resetval
-		-- See https://github.com/postgis/docker-postgis/issues/288
-		\c
-		CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
-		CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
+        echo "Loading PostGIS extensions into $DB"
+        "${psql[@]}" --dbname="$DB" <<-'EOSQL'
+                SET APPLICATION_NAME TO securedump;
+                CREATE EXTENSION IF NOT EXISTS postgis;
+                CREATE EXTENSION IF NOT EXISTS postgis_topology;
+                -- Reconnect to update pg_setting.resetval
+                -- See https://github.com/postgis/docker-postgis/issues/288
+                \c
+                SET APPLICATION_NAME TO securedump;
+                CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
+                CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
 EOSQL
 done
