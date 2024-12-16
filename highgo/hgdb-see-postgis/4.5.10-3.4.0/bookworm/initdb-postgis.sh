679f473cebcf5d5d745332976b06711a431a020e
RogerQiu <blatwo@163.com>
2024-12-17 01:29:07 +0800

    alter initdb-postgis.sh ignore dbname=sysdba

--- highgo/hgdb-see-postgis/4.5.10-3.4.0/bookworm/initdb-postgis.sh
+++ highgo/hgdb-see-postgis/4.5.10-3.4.0/bookworm/initdb-postgis.sh
@@ -14,10 +14,17 @@ EOSQL
 
 # Load PostGIS into both template_database and $POSTGRES_DB
 for DB in template_postgis "$POSTGRES_DB"; do
-        echo "Loading PostGIS extensions into $DB"
-        "${psql[@]}" --dbname="$DB" <<-'EOSQL'
-                SET APPLICATION_NAME TO securedump;
-                CREATE EXTENSION IF NOT EXISTS postgis;
-                --CREATE EXTENSION IF NOT EXISTS postgis_topology;
+    # 判断当前数据库是否为 sysdba，如果是则跳过
+    if [ "$DB" = "sysdba" ]; then
+        echo "Skipping PostGIS extension creation for sysdba database."
+        continue
+    fi
+
+    echo "Loading PostGIS extensions into $DB"
+    "${psql[@]}" --dbname="$DB" <<-'EOSQL'
+        SET APPLICATION_NAME TO securedump;
+        CREATE EXTENSION IF NOT EXISTS postgis;
+        --CREATE EXTENSION IF NOT EXISTS postgis_topology;
 EOSQL
 done
+
